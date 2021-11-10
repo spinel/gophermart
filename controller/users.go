97804444deb22908216566d435e48f105cc91a5d
Vladimir Kaydalin <exfinancier@gmail.com>
2021-11-11 02:07:08 +0300

    login after register

--- controller/users.go
+++ controller/users.go
@@ -35,7 +35,37 @@ func (ctr *Controller) RegisterUser(c echo.Context) error {
 
 	ctr.logger.Debug().Msgf("Created user '%d'", createdUser)
 
+	ctr.userAuth(c, userRegisterForm)
+
 	return c.JSON(http.StatusOK, createdUser)
+}
+
+// UserLogin handle user sign in
+func (ctr *Controller) userAuth(c echo.Context, userRegisterForm model.UserRegisterForm) error {
+	user, err := ctr.services.User.Login(ctr.ctx, userRegisterForm)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Errorf("user could not login"))
+	}
+
+	if user == nil {
+		return echo.NewHTTPError(http.StatusUnauthorized, fmt.Errorf("permission denied"))
+	}
+
+	// create new token
+	sessionToken := createSessionToken()
+
+	// Set the token in the cache, along with the user whom it represents
+	ctr.services.Memory.Add(ctr.ctx, sessionToken, user.ID)
+
+	// Finally, we set the client cookie for "session_token" as the session token we just generated
+	// we also set an expiry time of 120 seconds, the same as the cache
+	http.SetCookie(c.Response().Writer, &http.Cookie{
+		Name:    "session_token",
+		Value:   sessionToken,
+		Expires: time.Now().Add(1200 * time.Second),
+	})
+
+	return nil
 }
 
 // UserLogin handle user sign in
