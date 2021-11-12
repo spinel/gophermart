a80cec45fd1bd8c24cdd8a1c08b6347d3ccbecd6
Vladimir Kaydalin <exfinancier@gmail.com>
2021-11-12 19:40:03 +0300

    clear

--- cmd/gophermart/runbuild.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source ./env.sh
-go build ./*.go
--- model/preorder.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Preorder struct {
-	Base
-	ID     int     `json:"-" pg:"id,notnull,pk"`
-	Number  string  `json:"number" validate:"required" pg:"number,unique,notnull"`
-	Amount float64 `json:"amount" pg:"amount"`
-}
